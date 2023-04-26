d4a547f52c4f5ebca33a3a63e5c6371c76905258
Taufeeq <taufeeq.riyaz64@gmail.com>
2023-04-26 13:07:28 +0530

    /

--- app.js
+++ app.js
@@ -1,18 +1,61 @@
-var	express = require('express'),
-	app	= express();
+var express		= require('express'),
+	mongoose	= require('mongoose'),
+	bodyParser	= require('body-parser'),
+	app		= express();
 
+/********	APP SETUP	********/
 // Set port using environment variable PORT, otherwise 3000
 app.set('port', (process.env.PORT || 3000));
 
 // Set up engine to look for '.ejs' files.
 app.set('view engine', 'ejs');
 
- // Home route
- app.get('/', function(req, res) {	
- 	res.send('Hello!');
- });
- 
- // Start up server
- app.listen(app.get('port'), function() {
- 	console.log('App running on ', app.get('port'));
- });
+// Set up app to use body-parser
+app.use(bodyParser.urlencoded({ extended:true }));
+
+
+/********	DB SETUP	********/
+// Set up mongoose
+mongoose.connect('mongodb://localhost/men_stack_example');
+mongoose.promise = Promise;
+
+// define model
+var entrySchema = new mongoose.Schema({
+	value: {
+		type: String
+	}
+});
+var Entries = mongoose.model('Entries', entrySchema);
+
+
+/********	ROUTES	********/
+// Home route
+app.get('/', function(req, res) {
+	Entries.find()
+	.then( function(entries) {
+		res.render('home', {entries: entries});
+	})
+	.catch( function(err) {
+		res.send(err);
+	});
+});
+
+app.post('/add-entry', function(req, res) {
+	var entry = {
+		value: req.body.value
+	};
+
+	Entries.create(entry)
+	.then( function(entry) {
+		res.redirect('/')
+	})
+	.catch( function(err) {
+		res.send(err);
+	});
+})
+
+
+// Start up server
+app.listen(app.get('port'), function() {
+	console.log('App running on ', app.get('port'));
+});
